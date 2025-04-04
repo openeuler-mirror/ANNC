641b60177a7df2734b055fb2aa6a148f1f8beb6e
Pablo 00855658 <pablo.antonio.martinez@huawei.com>
2025-04-04 23:11:30 +0800

    Add script to apply the LLVM patches to tfserving as well

---
+++ install/tfserver/llvm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This should point to your annc path!
+YOUR_ANNC_PATH=
+PATH_OF_PATCHES="$ANNC_PATH/install/llvm/"
+
+cat $PATH_OF_PATCHES/annc1.patch | tail -n +7 | cut -c 2- > tmp.patch && patch -p1 < tmp.patch; rm tmp.patch
+cat $PATH_OF_PATCHES/annc2.patch | tail -n +7 | cut -c 2- > tmp.patch && patch -p1 < tmp.patch; rm tmp.patch
+cat $PATH_OF_PATCHES/annc3.patch | tail -n +7 | cut -c 2- > tmp.patch && patch -p1 < tmp.patch; rm tmp.patch
+cat $PATH_OF_PATCHES/annc4.patch | tail -n +7 | cut -c 2- > tmp.patch && patch -p1 < tmp.patch; rm tmp.patch
+
