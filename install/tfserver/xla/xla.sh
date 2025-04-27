7883cc62653d76da73b6bee2b3930a28e6959306
cuixiaofeng <cuixiaofeng1@huawwei.com>
2025-04-27 14:52:45 +0800

    refactor xla patches

---
+++ install/tfserver/xla/xla.sh
@@ -0,0 +1,5 @@
+PATH_OF_PATCHES="$ANNC_PATH/install/xla"
+patch -p1 < $PATH_OF_PATCHES/cpu.patch
+patch -p1 < $PATH_OF_PATCHES/mlir_hlo.patch
+patch -p1 < $PATH_OF_PATCHES/option.patch
+patch -p1 < $PATH_OF_PATCHES/debug.patch
