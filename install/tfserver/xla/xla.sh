d12b0e00763c4eb21d0d7b022bbee42e5d00ba6b
cuixiaofeng <cuixiaofeng1@huawwei.com>
2025-04-28 10:20:39 +0800

    support matmul custom call in mlir

--- install/tfserver/xla/xla.sh
+++ install/tfserver/xla/xla.sh
@@ -3,3 +3,4 @@ patch -p1 < $PATH_OF_PATCHES/cpu.patch
 patch -p1 < $PATH_OF_PATCHES/mlir_hlo.patch
 patch -p1 < $PATH_OF_PATCHES/option.patch
 patch -p1 < $PATH_OF_PATCHES/debug.patch
+patch -p1 < $PATH_OF_PATCHES/matmul_lower.patch
