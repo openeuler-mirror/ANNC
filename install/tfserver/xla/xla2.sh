0a15e4f062c31562903c593929145ad96e5c1357
Wen Di <wendi5@huawei.com>
2025-08-14 01:58:33 +0000

    update install/tfserver/xla/xla2.sh.
    Signed-off-by: Wen Di <wendi5@huawei.com>

--- install/tfserver/xla/xla2.sh
+++ install/tfserver/xla/xla2.sh
@@ -1,19 +1,13 @@
 TF_PATH=""
-ORG_TF_PATH=""
 XLA_PATH=""
 
 TF_PATCH_PATH="$ANNC/install"
-if [ ! -d "${ORG_TF_PATH}" ]; then
-    echo "The ORG_TF_PATH should be set."
-fi
-cd $ORG_TF_PATH
-patch -p1 < $TF_PATCH_PATH/tensorflow.patch
-
 PATH_OF_PATCHES="$ANNC/install/xla"
 if [ ! -d "${TF_PATH}" ]; then
     echo "The TF_PATH should be set."
 fi
 cd $TF_PATH
+patch -p1 < $TF_PATCH_PATH/tensorflow.patch
 patch -p1 < $PATH_OF_PATCHES/cpu_compiler.patch
 patch -p1 < $PATH_OF_PATCHES/simple_orc_jit.cc.patch
 patch -p1 < $PATH_OF_PATCHES/cpu_runtime.h.patch
