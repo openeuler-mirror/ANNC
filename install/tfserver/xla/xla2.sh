ce9dbfd5dc69519948bf3d1e93cc2eb15ea73360
Wen Di <wendi5@huawei.com>
2025-08-14 02:21:50 +0000

    update install/tfserver/xla/xla2.sh.
    Signed-off-by: Wen Di <wendi5@huawei.com>

--- install/tfserver/xla/xla2.sh
+++ install/tfserver/xla/xla2.sh
@@ -8,13 +8,6 @@ if [ ! -d "${TF_PATH}" ]; then
 fi
 cd $TF_PATH
 patch -p1 < $TF_PATCH_PATH/tensorflow.patch
-patch -p1 < $PATH_OF_PATCHES/cpu_compiler.patch
-patch -p1 < $PATH_OF_PATCHES/simple_orc_jit.cc.patch
-patch -p1 < $PATH_OF_PATCHES/cpu_runtime.h.patch
-patch -p1 < $PATH_OF_PATCHES/cpu_runtime.cc.patch
-patch -p1 < $PATH_OF_PATCHES/ir_emitter.h.patch
-patch -p1 < $PATH_OF_PATCHES/ir_emitter.cc.patch
-patch -p1 < $PATH_OF_PATCHES/cpu_instruction_fusion.h.patch
 
 if [ ! -d "${XLA_PATH}" ]; then
     echo "The XLA_PATH should be set."
@@ -24,3 +17,10 @@ patch -p1 < $PATH_OF_PATCHES/BUILD.patch
 patch -p1 < $PATH_OF_PATCHES/debug_options_flags.cc.patch
 patch -p1 < $PATH_OF_PATCHES/hlo_xla_runtime_pipeline.h.patch
 patch -p1 < $PATH_OF_PATCHES/xla.proto.patch
+patch -p1 < $PATH_OF_PATCHES/cpu_compiler.patch
+patch -p1 < $PATH_OF_PATCHES/simple_orc_jit.cc.patch
+patch -p1 < $PATH_OF_PATCHES/cpu_runtime.h.patch
+patch -p1 < $PATH_OF_PATCHES/cpu_runtime.cc.patch
+patch -p1 < $PATH_OF_PATCHES/ir_emitter.h.patch
+patch -p1 < $PATH_OF_PATCHES/ir_emitter.cc.patch
+patch -p1 < $PATH_OF_PATCHES/cpu_instruction_fusion.h.patch
