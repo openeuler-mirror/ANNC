9c943996171a4fcd23576eb83749998da7af921c
Wen Di <wendi5@huawei.com>
2025-08-19 21:17:10 +0800

    Add ENABLE_ANNC option for xla

--- install/tfserver/xla/xla2.sh
+++ install/tfserver/xla/xla2.sh
@@ -2,15 +2,16 @@ TF_PATCH_PATH="$ANNC/install"
 PATH_OF_PATCHES="$ANNC/install/xla"
 if [ ! -d "${TF_PATH}" ]; then
     echo "The TF_PATH should be set."
+    exit
 fi
 cd $TF_PATH
 patch -p1 < $TF_PATCH_PATH/tensorflow.patch
 
 if [ ! -d "${XLA_PATH}" ]; then
     echo "The XLA_PATH should be set."
+    exit
 fi
 cd $XLA_PATH
-patch -p1 < $PATH_OF_PATCHES/BUILD.patch
 patch -p1 < $PATH_OF_PATCHES/debug_options_flags.cc.patch
 patch -p1 < $PATH_OF_PATCHES/hlo_xla_runtime_pipeline.h.patch
 patch -p1 < $PATH_OF_PATCHES/xla.proto.patch
