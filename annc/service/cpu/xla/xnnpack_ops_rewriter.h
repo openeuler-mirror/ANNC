ae1c3261ce5c55aa9e0c73ef5b25851ce268ed3f
Wen Di <wendi5@huawei.com>
2025-09-08 14:47:51 +0800

    Add licenses

--- annc/service/cpu/xla/xnnpack_ops_rewriter.h
+++ annc/service/cpu/xla/xnnpack_ops_rewriter.h
@@ -1,4 +1,5 @@
-/* Copyright 2025 Huawei. All Rights Reserved.
+/* Referenced & Modified External Open Source Code:
+Original Copyright: 2023 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
