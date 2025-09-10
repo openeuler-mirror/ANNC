0516126c3ccc050e835a2dd37f440f65344ab06d
Wen Di <wendi5@huawei.com>
2025-09-10 03:17:35 +0000

    update annc/service/cpu/xla/xnnpack_ops_rewriter.cc.
    Signed-off-by: Wen Di <wendi5@huawei.com>

--- annc/service/cpu/xla/xnnpack_ops_rewriter.cc
+++ annc/service/cpu/xla/xnnpack_ops_rewriter.cc
@@ -1,7 +1,4 @@
 /*
-Referenced & Modified External Open Source Code:
-Original Copyright: 2023 The OpenXLA Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
